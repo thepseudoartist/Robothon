55ac96679d3a171b606f2c887206323dd818631e
Rishabh <immrishabh@gmail.com>
2020-02-29 18:50:26 +0530

    UI

--- app/src/main/java/blueshark/app/robothon/activities/BaseActivity.java
+++ app/src/main/java/blueshark/app/robothon/activities/BaseActivity.java
@@ -1,10 +1,15 @@
 package blueshark.app.robothon.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.mapbox.mapboxsdk.Mapbox;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -17,6 +22,40 @@ public abstract class BaseActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                              WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        init();
+    }
+
+    private void init(){
+        Mapbox.getInstance(this, "pk.eyJ1IjoidGhlcHNldWRvYXJ0aXN0IiwiYSI6ImNrNzcyZHY5dDAzM2czZnIxOWczdGtiYzIifQ.jy4ijsk1BK-XIKGl6fendA");
+
+        getPermission();
+    }
+
+    private void getPermission() {
+        boolean permissionAccessCoarseLocationApproved =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED;
+
+        if (permissionAccessCoarseLocationApproved) {
+            boolean backgroundLocationPermissionApproved =
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED;
+
+            if (!backgroundLocationPermissionApproved) {
+                ActivityCompat.requestPermissions(this, new String[] {
+                                Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                        9005);
+            }
+            
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    }, 9005);
+        }
+
     }
 
     protected abstract int getLayoutResourceId();
